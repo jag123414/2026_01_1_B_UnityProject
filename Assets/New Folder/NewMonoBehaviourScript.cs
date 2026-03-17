40a5522f0bd3be8520895e766ef8863ee7c12e00
jag123414 <a01040264560@gmail.com>
2026-03-17 12:13:32 +0900

    [Feat] 유니티 기초 코딩
    타이머설정, 변수 설정, 키보드 설정

---
+++ Assets/New Folder/NewMonoBehaviourScript.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class MyCharacter : MonoBehaviour
+{
+    public int Health = 100;
+    public float Timer = 1.0f;
+    void Start()
+    {
+        Health = Health + 100;
+    }
+
+    
+    void Update()
+    {
+        Timer = Timer - Time.deltaTime;
+
+        if (Timer < 0)
+        {
+            Timer = 1.0f;
+            Health = Health - 20;
+        }
+
+        if (lnput.GetKeyDown(KeyCode.Space))
+        {
+            Hea1th = Health + 2;
+        }
+
+        if (Health < 0)
+        {
+            Destroy(this.game0bject);
+        }
+    }
+}
---
+++ Assets/New Folder/jm.cs
@@ -0,0 +1,32 @@
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class jm : MonoBehaviour
+{
+    Rigidbody rigidbody;
+    public float power = 200f;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        timer = timer + Time.deltaTime;
+        TextUI.text = timer.ToString();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            power = power + Random.Range(-100, 200);
+            rigidbody.AddForce(transform.up * power);
+        }
+
+        if (this.gameObject.transform.position.y > 5 || this.gameObject.transform.position.y < -3)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
